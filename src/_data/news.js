19338c15dc2bec8bb9b41feed6b8044fcfd28d3f
hachtech <hachtech07@gmail.com>
2020-11-26 20:31:08 +0545

    "more change"

--- src/_data/news.js
+++ src/_data/news.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 module.exports = async function getNews(){
     try{
-        const response = await axios.get(`https://newsapi.org/v2/top-headlines/?sources=google-news&apiKey=${process.env.API_KEY}`);
+        const response = await axios.get(`https://newsapi.org/v2/top-headlines/?sources=google-news&apiKey=${process.env.NEWS_API_KEY}`);
        // console.log(response);
        return response.data;
     }catch(error){
